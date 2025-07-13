0d13172ef01a834510206d202f663770bd24a6ae
marwan562 <mixing.gamer546@gmail.com>
2025-07-13 19:48:11 +0300

    fix: Improve error handling in validation middleware by joining error messages and adding status code

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,12 +1,13 @@
 import { AppError } from "../utilities/AppError.js";
 
 export const validation = (schema) => {
-  return (req, _, next) => {
-    let input = { ...req.body, ...req.params, ...req.query };
-    let { error } = schema.validate(input, { abortEarly: false });
+  return (req, res, next) => {
+    const input = { ...req.body, ...req.params, ...req.query };
+    const { error } = schema.validate(input, { abortEarly: false });
+
     if (error) {
-      let errors = error.details.map((detail) => detail.message);
-      return next(new AppError(errors));
+      const errors = error.details.map((detail) => detail.message);
+      return next(new AppError(errors.join(", "), 400));
     }
     next();
   };

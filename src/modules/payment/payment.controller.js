5389f32307d7bc07a760cbb68d01bb9e4f24bb14
marwan562 <mixing.gamer546@gmail.com>
2025-11-29 05:08:30 +0200

    chore:(payment) update api version

--- src/modules/payment/payment.controller.js
+++ src/modules/payment/payment.controller.js
@@ -13,7 +13,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error("STRIPE_SECRET_KEY is not defined");
 }
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-08-27.basil",
+  apiVersion: "2025-07-30.basil",
 });
 
 dotenv.config();
